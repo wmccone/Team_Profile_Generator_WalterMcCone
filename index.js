aaf62a577306d526b821b2fa4aa2dd17c3cb0990
Gerry M <waltermccone88@gmail.com>
2021-03-09 21:31:46 -0600

    updated employee classes and started creating the prompt

--- index.js
+++ index.js
@@ -2,3 +2,42 @@ const Employee = require('./lib/employee')
 const Manager = require('./lib/manager')
 const Engineer = require('./lib/engineer')
 const Intern = require('./lib/intern')
+const Inquirer = require('inquirer');
+
+Inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of your manager?',
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is the employee id of your manager?',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the managers email?',
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: 'What is the office number for the manager?',
+        },
+    ])
+    .then()
+
+
+
+
+
+function init() {
+//Welcome message
+    console.log("Welcome to the Team Profile generator, lets build a team!")
+// prompt user for manager information
+// prompt user for name of next employee
+// prompt user for 
+    
+}
